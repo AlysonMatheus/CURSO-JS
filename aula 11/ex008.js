6e5da43de3b3dd91159ce7ec9e6903fd817c6837
AlysonMatheus <alyson2000@live.com>
2023-08-03 17:31:58 -0300

    Composição simples/aninhadas/multiplas

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade  do seu carro é ${vel} km/h`)
+if (vel > 60) // condição simples 
+ {
+    console.log('Voce ultrapassou o limite de velocidade. MULTADO!!')
+}
+console.log ('Diriga sempre usando sinto de segurança')
